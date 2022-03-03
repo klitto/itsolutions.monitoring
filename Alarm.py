4325cefcf4118b3ea687def3cd3b5d15c7f410ae
klitto <99952188+klitto@users.noreply.github.com>
2022-03-03 13:28:19 +0100

    Added a class to Alarm.py

--- Alarm.py
+++ Alarm.py
@@ -1,15 +1,17 @@
 import smtplib
 from email.message import EmailMessage
 
-def sendAlarmMail(mail_login, mail_to, mail_password, mail_server, subject, message):
-    msg = EmailMessage()
+class Alarm():
 
-    msg["Subject"] = subject
-    msg["From"] =  mail_login
-    msg["To"] = mail_to
-    msg.set_content(message)
+    def sendAlarmMail(mail_login, mail_to, mail_password, mail_server, subject, message):
+        msg = EmailMessage()
 
-    with smtplib.SMTP_SSL(mail_server, 465) as smtp:
-        smtp.login(mail_login, mail_password)
+        msg["Subject"] = subject
+        msg["From"] =  mail_login
+        msg["To"] = mail_to
+        msg.set_content(message)
 
-        smtp.send_message(msg)
+        with smtplib.SMTP_SSL(mail_server, 465) as smtp:
+            smtp.login(mail_login, mail_password)
+
+            smtp.send_message(msg)

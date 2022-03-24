1b13e9c43a5d9b06197549fd7dccb5b71a358750
BuildTools <unconfigured@null.spigotmc.org>
2022-03-24 20:18:44 +0100

    measured values ​​from unittesting are logged from now on

--- Unittesting.py
+++ Unittesting.py
@@ -7,29 +7,37 @@ import psutil
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO, stream=sys.stdout)
 
 class MonitoringTestCase(unittest.TestCase):
-       
+
+    '''
+    Compares the results of the monitoring class functions with a separate check
+    '''
     def testRAMUsage(self):
         testObject = Monitoring()
         ergebnis = testObject.checkRAMUsage(unittest=True)
         self.assertLess(0, ergebnis)
+        print("Gemessener RAM-Testwert: " + ergebnis)
     def testCPUUsage(self):
         testObject = Monitoring()
         ergebnis = testObject.checkCPUUsage(unittest=True)
         self.assertLess(0, ergebnis)
+        print("Gemessener CPU-Testwert: " + ergebnis)
     def testDiskUsage(self):
         testObject = Monitoring()
         ergebnis = testObject.checkDiskUsage(unittest=True)
         self.assertGreater(101, ergebnis)
         self.assertEqual(ergebnis, psutil.disk_usage("/")[3])
+        print("Gemessener Disk-Usage-Testwert: " + ergebnis)
     def testProcesses(self):
         testObject = Monitoring()
         ergebnis = testObject.checkProcesses(unittest=True)
         self.assertLess(0, ergebnis)
         self.assertEqual(ergebnis, len(psutil.pids()))
+        print("Gemessener Prozess-Testwert: " + ergebnis)
     def testStartTime(self):
         testObject = Monitoring()
         ergebnis = testObject.checkStartTime(unittest=True)
         self.assertEqual(ergebnis, round(int(time.time() - psutil.boot_time())  / 100 / 60 / 60))
+        print("Gemessener Starttime-Testwert: " + ergebnis)
 
 if __name__ == "__main__":
     unittest.main()
